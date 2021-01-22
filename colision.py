c3f47467241e7455ec2b1a6ff8bc0b361dc08ebc
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-01-22 14:21:44 +0100

    done collision pygame

--- colision.py
+++ colision.py
@@ -12,31 +12,37 @@ screen = pygame.display.set_mode((width, height))
 
 moving_rect = pygame.Rect(50,50,100,100)
 x_speed, y_speed = 5,4
+other_speed = 2
 
 other_rect = pygame.Rect(300,200, 100,100)
 
 
 def bouncing_rect():
-    global  x_speed, y_speed
+    global  x_speed, y_speed, other_speed
     pygame.draw.rect(screen, (255, 255, 255), moving_rect)
     pygame.draw.rect(screen, (255, 0, 0), other_rect)
 
     moving_rect.x += x_speed
     moving_rect.y += y_speed
+    other_rect.y += other_speed
+
+    if other_rect.top<=0 or other_rect.bottom >= height:
+        other_speed*=-1
+
 
     #basic checking collision
     collision_tollerance = 10
     if moving_rect.colliderect(other_rect):
-        if abs(moving_rect.top - other_rect.bottom) <= collision_tollerance:
+        if abs(moving_rect.top - other_rect.bottom) <= collision_tollerance and y_speed<0:
             y_speed*=-1
 
-        if abs(moving_rect.bottom - other_rect.top)<= collision_tollerance:
+        if abs(moving_rect.bottom - other_rect.top)<= collision_tollerance and y_speed>0:
             y_speed*=-1
 
-        if abs(moving_rect.left - other_rect.right) <= collision_tollerance:
+        if abs(moving_rect.left - other_rect.right) <= collision_tollerance and x_speed <0:
             x_speed*=-1
 
-        if abs(moving_rect.right - other_rect.left) <= collision_tollerance:
+        if abs(moving_rect.right - other_rect.left) <= collision_tollerance and x_speed >0 :
             x_speed*=-1
 
     if moving_rect.right >= width or moving_rect.left<=0:
