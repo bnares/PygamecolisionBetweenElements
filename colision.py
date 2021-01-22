6871f449b038d47301a7f561179fed5c3c821d8b
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-01-22 09:28:28 +0100

    first rectangle

--- colision.py
+++ colision.py
@@ -10,6 +10,11 @@ width = 600
 height = 600
 screen = pygame.display.set_mode((width, height))
 
+moving_rect = pygame.Rect(350,350,100,100)
+x_speed, y_speed = 5,4
+
+other_rect = pygame.Rect(300,200, 200,100)
+
 
 while True:
     for event in pygame.event.get():
@@ -17,6 +22,10 @@ while True:
             pygame.quit()
             sys.exit()
 
+
+
+
     screen.fill((30, 30, 30))
+    pygame.draw.rect(screen, (255, 255, 255), moving_rect)
     pygame.display.update()
     clock.tick(60)
